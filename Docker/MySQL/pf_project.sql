c525a5772edd552cf4bb6b7ee77769a0954b008d
denizcamalan <denizcamalann@gmail.com>
2022-08-12 02:48:39 +0300

    update dockerfile

---
+++ Docker/MySQL/pf_project.sql
@@ -0,0 +1,31 @@
+Use pf_project;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+
+CREATE TABLE `cart` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `vat` float UNSIGNED NOT NULL,
+  `price` float UNSIGNED NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `quantity` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `products` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `price` float UNSIGNED NOT NULL,
+  `vat` float UNSIGNED NOT NULL,
+  `quantity` int(10) UNSIGNED NOT NULL,
+  `description` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `products` (`id`, `name`, `price`, `vat`, `quantity`, `description`) VALUES
+(3, 'Apple 13 Pro', 1025.6, 0.08, 23, 'phone'),
+(4, 'Arcelik Hg567', 235.3, 0.08, 12, 'refrigerator'),
+(8, 'Beko Ecological Inverter', 510.5, 0.1, 18, 'air conditioner'),
+(7, 'Hyundai Inverter', 1400.9, 0.08, 5, 'generator'),
+(6, 'JBL', 80.7, 0.1, 39, 'headphone'),
+(1, 'Macbook Air', 1230.6, 0.18, 20, 'computer'),
+(2, 'Samsung S5', 650.4, 0.18, 14, 'phone'),
+(5, 'Vestel Smart', 350.2, 0.1, 30, 'tv');
--- src/database/config.go
+++ src/database/config.go
@@ -2,25 +2,17 @@ package database
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func Get_db() (db *sql.DB, err error) {
 
-	// dataBaseSystem := "mysql"
-	// dbName := "pf_project"
-	// dbUser := "root"
-	// dbPass := "root"
-	// db, err = sql.Open(dataBaseSystem, dbUser+":"+dbPass+"@tcp(172.22.0.1:8889)/"+dbName)
-
-	db, err = sql.Open("mysql","root:root@tcp(172.22.0.2:8888)/pf_project")
+	db, err = sql.Open("mysql","root:password@tcp(localhost:3306)/pf_project")
 	if err != nil {
-		return db,err
+		log.Println(err)
 	}
-	//db.SetMaxIdleConns(-1)
-	//db.SetConnMaxLifetime(time.Second)
-	db.Ping()	
 
-	return  db, nil
+	return 
 }
